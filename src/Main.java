d45c927bd42ebb89552d6374067378d54d7a27ea
MANISH LANJEWAR <manishlanjewar8@gmail.com>
2022-09-29 18:34:45 +0530

    Added:Uc5_monthlyWage

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,43 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        float dailyWage;
-        int wagePerHour = 20, fullDayHour = 0;
+        float dailyWage, monthlyWage = 0;
+        int wagePerHour = 20, fullDayHour = 0, workingDaysPerMonth = 20;
 
         System.out.println("Welcome to Employee Wage Computation Program.");
 
         Random random = new Random();
-        int attendance = random.nextInt(2);
-        System.out.println(attendance);
-        int isPartTime = 1 + random.nextInt(2);
+        int attendance;
 
-        switch (attendance) {
-            case 1:
-                System.out.println("Employee is present!");
-                switch (isPartTime) {
-                    case 1:
-                        System.out.println("Employee is part time.");
-                        fullDayHour = 4;
-                        break;
-                    case 2:
-                        System.out.println("Employee is full time.");
-                        fullDayHour = 8;
-                }
-                break;
-            case 0:
-                System.out.println("Employee is absent.");
+        int isPartTime;
 
+        for (int i = 1; i <= workingDaysPerMonth; i++) {
+            attendance = random.nextInt(2);
+            System.out.println(attendance);
+            isPartTime = 1 + random.nextInt(2);
+
+            switch (attendance) {
+                case 1:
+                    System.out.println("Employee is present!");
+                    switch (isPartTime) {
+                        case 1:
+                            System.out.println("Employee is part time.");
+                            fullDayHour = 4;
+                            break;
+                        case 2:
+                            System.out.println("Employee is full time.");
+                            fullDayHour = 8;
+                    }
+                    break;
+                case 0:
+                    System.out.println("Employee is absent.");
+
+            }
+
+            dailyWage = wagePerHour * fullDayHour;
+            monthlyWage += dailyWage;
+            System.out.println("Day " + i + " wage of employee: " + dailyWage + "\n");
         }
-
-        dailyWage = wagePerHour * fullDayHour;
-        System.out.println("Daily wage of employee: " + dailyWage);
-
+        System.out.println("\nMonth wage of employee: " + monthlyWage);
     }
 }
