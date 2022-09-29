491a8041b5f4e9a05869629114a3575d51101e3a
MANISH LANJEWAR <manishlanjewar8@gmail.com>
2022-09-29 18:05:01 +0530

    Added:Uc1_checkAttendance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee wage computation");
+       Random random = new Random();
+        boolean attendance = random.nextBoolean();
+        System.out.println(attendance);
+        if (attendance)
+            System.out.println("Employee is present.");
+        else
+            System.out.println("Employee is absent.");
     }
 }
