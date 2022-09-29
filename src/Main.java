07b3867394cbf602534bedd1741f562f1f199a99
MANISH LANJEWAR <manishlanjewar8@gmail.com>
2022-09-29 18:28:31 +0530

    Changed to switchCase

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
         float dailyWage;
         int wagePerHour = 20, fullDayHour = 0;
@@ -9,23 +8,30 @@ public class Main {
         System.out.println("Welcome to Employee Wage Computation Program.");
 
         Random random = new Random();
-        boolean attendance = random.nextBoolean();
+        int attendance = random.nextInt(2);
         System.out.println(attendance);
+        int isPartTime = 1 + random.nextInt(2);
 
-        boolean isPartTime = random.nextBoolean();
-        if (attendance)
-            if (isPartTime) {
-                System.out.println("Part time employee is present.");
-                fullDayHour = 4;
-            }
-            else {
-                System.out.println("Full time employee is present.");
-                fullDayHour = 8;
-            }
-        else
-            System.out.println("Employee is absent.");
+        switch (attendance) {
+            case 1:
+                System.out.println("Employee is present!");
+                switch (isPartTime) {
+                    case 1:
+                        System.out.println("Employee is part time.");
+                        fullDayHour = 4;
+                        break;
+                    case 2:
+                        System.out.println("Employee is full time.");
+                        fullDayHour = 8;
+                }
+                break;
+            case 0:
+                System.out.println("Employee is absent.");
+
+        }
 
         dailyWage = wagePerHour * fullDayHour;
         System.out.println("Daily wage of employee: " + dailyWage);
+
     }
 }
