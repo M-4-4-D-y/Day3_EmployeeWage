12f2ed0746bec8c7e4cb1be13d45eb383e081819
MANISH LANJEWAR <manishlanjewar8@gmail.com>
2022-09-29 18:12:39 +0530

    Added: Uc2_dailyWage

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,22 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee wage computation");
-       Random random = new Random();
+        float dailyWage;
+        int wagePerHour = 20, fullDayHour = 0;
+
+        System.out.println("Welcome to Employee Wage Computation Program.");
+
+        Random random = new Random();
         boolean attendance = random.nextBoolean();
         System.out.println(attendance);
-        if (attendance)
+        if (attendance) {
             System.out.println("Employee is present.");
+            fullDayHour = 8;
+        }
         else
             System.out.println("Employee is absent.");
+
+        dailyWage = wagePerHour * fullDayHour;
+        System.out.println("Daily wage of employee: " + dailyWage);
     }
 }
